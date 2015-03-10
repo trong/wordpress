cb25264e5e6d1cdffb7e55522138b43118553501
Vadim Madison <vmadison@rutube.ru>
2015-03-10 23:15:23 +0300

    Empty tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Rutube PHP API Client package.
+ *
+ * (c) Rutube
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+//require_once __DIR__ . '/../vendor/autoload.php';
+
+const USER_LOGIN = 'squirrel@squirrel.ru';
+const USER_PASS = '998800';
+const RUTUBE_HOST = 'rutube.ru';
